4d2c1fc158490896d242d5214c85b25fd2c10112
Alexey Andreev <dark-cross@rambler.ru>
2021-02-21 18:35:00 +0500

    add manager file

--- src/main/java/manager/TicketManager.java
+++ src/main/java/manager/TicketManager.java
@@ -1,5 +1,44 @@
 package manager;
 
+import domain.Ticket;
+import repository.TicketRepository;
+
+import java.util.Arrays;
+
 public class TicketManager {
+    private TicketRepository repository;
+
+    public TicketManager() {
+    }
+
+    public TicketManager(TicketRepository repository) {
+        this.repository = repository;
+    }
+
+    public void add (Ticket ticket) {
+        repository.save(ticket);
+
+    }
+
+    public void removeById (int id) {
+        repository.removeById(id);
+    }
+
+    public Ticket [] findAll(String from, String to) {
+        Ticket [] result = new Ticket[0];
+        for (Ticket ticket : repository.findAll()) {
+            if (from.equals(ticket.getFromAirport()) && to.equals(ticket.getToAirport())) {
+
+                Ticket[] tmp = new Ticket[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+
+                tmp[tmp.length -1] = ticket;
+                result = tmp;
+            }
+
+        }
+        Arrays.sort(result);
+        return result;
+    }
 
 }
